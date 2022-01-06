ba7445d1142669b27496c14ed319afdc4b3f214e
Erodynamics <Erodynamics@users.noreply.github.com>
2022-01-06 15:10:35 -0500

    Created prepacked VPKs for a more stable install

---
+++ patchForJapanese.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+cd "R2Northstar/mods/Northstar.Client/mod/resource"
+copy "northstar_client_localisation_japanese.txt" "northstar_client_localisation_japanese.txt.backup"
+copy "northstar_client_localisation_english.txt" "northstar_client_localisation_japanese.txt"
---
+++ restoreJapanese.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+cd "R2Northstar/mods/Northstar.Client/mod/resource"
+copy "northstar_client_localisation_japanese.txt.backup" "northstar_client_localisation_japanese.txt"
